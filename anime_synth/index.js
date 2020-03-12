e5087d164c97cc79084360582b67ff4181f120cb
Alexander Babel <alexandermbabel@protonmail.com>
2020-03-12 18:07:40 -0400

    circle_exp webpack & eslint setup

---
+++ anime_synth/index.js
@@ -0,0 +1,13 @@
+const anime = require('animejs');
+
+const container = document.createElement('DIV');
+const circle1 = document.createElement('DIV');
+const circle2 = document.createElement('DIV');
+const circle3 = document.createElement('DIV');
+
+document.body.appendChild(container);
+document.body.container.appendChild(cirlce1);
+document.body.container.circle1.appendChild(circle2);
+document.body.container.circle1.circle2.appendChild(circle3);
+
+console.log('sometihng');
