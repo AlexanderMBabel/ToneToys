cb5561117b7e16ea8793e3162b9a8749bd386648
Alexander Babel <alexandermbabel@protonmail.com>
2020-03-12 20:27:10 -0400

    added circle instrument react

---
+++ circle_exp/src/index.js
@@ -0,0 +1,8 @@
+const container = document.createElement('div', 'something');
+const circle = document.createElement('div');
+container.innerHTML = 'something';
+document.body.appendChild(container);
+
+document.body.appendChild(circle);
+document.body.appendChild(circle);
+document.body.appendChild(circle);
